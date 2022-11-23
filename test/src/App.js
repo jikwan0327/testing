0e4f28cf2211a0aa853b58868f005ccd33af5c15
jikwan <jikwan0327@gmail.com>
2022-11-23 20:27:19 +0900

    button & checkbox

--- test/src/App.js
+++ test/src/App.js
@@ -3,12 +3,27 @@ import "./App.css";
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
+
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
     <div>
-      <button onClick={() => setButtonColor("blue")} style={{ backgroundColor: buttonColor }}>
+      <button
+        disabled={disabled}
+        onClick={() => setButtonColor(newButtonColor)}
+        style={{ backgroundColor: buttonColor, color: "white" }}
+      >
         Change to {newButtonColor}
       </button>
+      <br />
+      <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
     </div>
   );
 }
